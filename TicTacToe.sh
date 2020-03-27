e30f57d1fd43a982b5e5c9cf40c1d4c308aaf246
Manasi2804 <shahmanasi281996@gmail.com>
2020-03-27 13:01:59 +0530

    Added:Check wining condition

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -23,7 +23,7 @@ printf"Enter any number bet 0-8:"
 read response
 if [[ $response>=0 && $response<=8 ]]
 then
-	if [[ "${gameboard[$response]}"!=X || "${gameboard[$response]}"!=o ]]
+	if [[ "${gameboard[$response]}"!=X || "${gameboard[$response]}"!=O ]]
 	then
 		gameboard[$response]="$playerletter"
 	else
@@ -34,7 +34,64 @@ else
 fi
 displayboard
 }
+function checkwin()
+{
+	letter=$1
+	if [ "${gameboard[0]}${gameboard[1]}${gameboard[2]}"=="$letter$letter$letter" ]
+	then
+		result="wins"
+	elif [ "${gameboard[3]}${gameboard[4]}${gameboard[5]}"=="$letter$letter$letter" ]
+	then
+		result="wins"
+	elif [ "${gameboard[6]}${gameboard[7]}${gameboard[8]}"=="$letter$letter$letter" ]
+	then
+		result="wins"
+	elif [ "${gameboard[0]}${gameboard[3]}${gameboard[6]}"=="$letter$letter$letter" ]
+	then
+		result="wins"
+	elif [ "${gameboard[1]}${gameboard[4]}${gameboard[7]}"=="$letter$letter$letter" ]
+	then
+		result="wins"
+	elif [ "${gameboard[2]}${gameboard[5]}${gameboard[8]}"=="$letter$letter$letter" ]
+	then
+		result="wins"
+	elif [ "${gameboard[0]}${gameboard[4]}${gameboard[8]}"=="$letter$letter$letter" ]
+	then
+		result="wins"
+	elif [ "${gameboard[2]}${gameboard[4]}${gameboard[6]}"=="$letter$letter$letter" ]
+	then
+		result="wins"
+	else
+		flag=0
+		for((index=0;index<${#gameboard[@]};index++))
+		do
+			if [[ "${gameboard[$response]}"!=X || "${gameboard[$response]}"!=O ]]
+			then
+				flag=1
+			fi
+		done
+		if [ $flag==0 ]
+		then
+			result="draw"
+		else
+			result="change"
+		fi
+	fi
+	echo $result
+}
 
+function assignletter(){
+	lettercheck=$((RANDOM%2))
+	case $lettercheck in
+		$Dot)
+			playerletter=$O
+			;;
+		$Cross)
+			playerLetter=$X
+			;;
+	esac
+	echo $playerletter
+}
 function getletter(){
 	lettercheck=$((RANDOM%2))
 	case $lettercheck in
@@ -59,6 +116,7 @@ function getturn(){
 			;;
 	esac
 }
-letter="$(getletter)"
+letter="$(assignletter)"
 getturn
 playerturn $letter
+checkwin $playerletter
