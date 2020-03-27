798d2c03e5d436f978dcea9a8a94bd031dd492a7
Manasi2804 <shahmanasi281996@gmail.com>
2020-03-27 10:36:42 +0530

    Added: Resetting the board

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -1,3 +1,12 @@
 #!/bin/bash -x
 
+declare -a gameboard
+gameboard=(" " " " " " " " " " " " " " " " " ")
+
 echo "Welcome to tic tac toe game"
+
+echo "${gameboard[0]}  | ${gameboard[1]} | ${gameboard[2]}"
+echo "-----------"
+echo "${gameboard[3]}  | ${gameboard[4]} | ${gameboard[5]}"
+echo "-----------"
+echo "${gameboard[6]}  | ${gameboard[7]} | ${gameboard[8]}"
